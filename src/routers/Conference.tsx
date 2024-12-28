ce65ffc3e81689f1f008abb52b56372f3b1169c4
javadocq <han18264@naver.com>
2024-12-28 22:17:11 +0900

    feat: ADD most of time logic

--- src/routers/Conference.tsx
+++ src/routers/Conference.tsx
@@ -6,7 +6,13 @@ const Container = styled.div`
   display: flex;
 `;
 
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const Comment = styled.div`
+  width: 700px;
   display: flex;
   flex-direction: column;
   position: relative;
@@ -61,10 +67,44 @@ const Button = styled.button`
   background: #fff;
   position: absolute;
   top: 33%;
-  left: 120%;
+  left: 80%;
+`;
+
+const MostOfTime = styled.div`
+  width: 1000px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MostOfTimeText = styled.p`
+  color: #717171;
+  text-align: center;
+  font-family: Inter;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin: 45px 0 4px 0;
+`;
+
+const Time = styled.p`
+  color: #000;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin: 15px 0 0 0;
 `;
 
 const Conference: React.FC = () => {
+  const max_respond_time: Record<number, string[]> = {
+    24: ["09 : 00 ~ 10 : 00", "10 : 00 ~ 11 : 00"],
+    26: ["19 : 00 ~ 20 : 00"],
+  };
+
   const handleCopyClipBoard = async () => {
     try {
       const currentUrl = window.location.href;
@@ -75,9 +115,19 @@ const Conference: React.FC = () => {
     }
   };
 
+  const formatTime = (time: string) => {
+    const [start, end] = time.split("~").map((t) => t.trim());
+    const getPeriod = (hour: number) => (hour < 12 ? "오전" : "오후");
+    const formatHour = (t: string) => {
+      const [hour, minute] = t.split(":").map(Number);
+      return `${getPeriod(hour)} ${hour % 12 < 10 ? "0" + (hour % 12) : hour % 12}:${minute.toString().padStart(2, "0")}`;
+    };
+    return `${formatHour(start)} ~ ${formatHour(end)}`;
+  };
+
   return (
     <Container>
-      <div>
+      <Wrap>
         <Comment>
           <Lable>제목</Lable>
           <Title>LCK 회의</Title>
@@ -86,11 +136,20 @@ const Conference: React.FC = () => {
           <Button onClick={() => handleCopyClipBoard()}>공유하기</Button>
         </Comment>
         <div></div>
-      </div>
-      <div>
+      </Wrap>
+      <Wrap>
+        <MostOfTime>
+          <MostOfTimeText>가장 많은 시간대</MostOfTimeText>
+          {Object.keys(max_respond_time).map((key: any) =>
+            max_respond_time[parseInt(key)].map((item, index) => (
+              <Time key={`${key}-${index}`}>
+                {key} : {formatTime(item)}
+              </Time>
+            ))
+          )}
+        </MostOfTime>
         <div></div>
-        <div></div>
-      </div>
+      </Wrap>
     </Container>
   );
 };
